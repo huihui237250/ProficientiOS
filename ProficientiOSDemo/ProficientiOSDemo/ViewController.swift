f463b7b7f89e2496b68b31412178809a41d9e196
glimmerlu <glimmerlu@caihuihuideMacBook-Air.local>
2016-07-12 20:43:46 +0800

    Create XCode Project

---
+++ ProficientiOSDemo/ProficientiOSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProficientiOSDemo
+//
+//  Created by glimmerlu on 16/7/12.
+//  Copyright © 2016年 huihui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
